392d15d9b33ceac182e1f9bf59f35bc1f64dd1a8
Ashish Kumar <3303ashish@gmail.com>
2026-08-12 00:44:37 +0530

    Time: 3 ms (61.57%) | Memory: 10.1 MB (78.95%) - LeetSync

--- Week_1/Day_1/51-n-queens/n-queens.cpp
+++ Week_1/Day_1/51-n-queens/n-queens.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftr,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n){
+    void queens(int col,vector<string> &ds,vector<vector<string>> &ans,vector<int> &leftr,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n){
         if(col==n){
-            ans.push_back(board);
+            ans.push_back(ds);
             return;
         }
         for(int row = 0;row<n;row++){
             if(leftr[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n-1 + col - row] == 0){
                 
-                board[row][col] = 'Q';
+                ds[row][col] = 'Q';
                 leftr[row] = 1;
                 lowerDiagonal[row+col] = 1;
                 upperDiagonal[n-1 + col - row] = 1;
-                solve(col+1,board,ans,leftr,upperDiagonal,lowerDiagonal,n);
-                board[row][col] = '.';
+                queens(col+1,ds,ans,leftr,upperDiagonal,lowerDiagonal,n);
+                ds[row][col] = '.';
                 leftr[row] = 0;
                 lowerDiagonal[row+col] = 0;
                 upperDiagonal[n-1 + col - row] = 0;
@@ -25,13 +25,13 @@ public:
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
+        vector<string> ds(n);
         string s(n,'.');
         for(int i=0;i<n;i++){
-            board[i]=s;
+            ds[i]=s;
         }
         vector<int> leftr(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
-        solve(0,board,ans,leftr,upperDiagonal,lowerDiagonal,n);
+        queens(0,ds,ans,leftr,upperDiagonal,lowerDiagonal,n);
         return ans;
           
 
