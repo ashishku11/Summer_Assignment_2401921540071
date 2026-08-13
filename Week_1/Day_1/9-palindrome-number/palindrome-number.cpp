38c97f32ca9c67d7c7b4f55e23c53f4fadb3bd94
Ashish Kumar <3303ashish@gmail.com>
2026-08-13 19:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.71%) - LeetSync

---
+++ Week_1/Day_1/9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int dup = x;
+        long long rev = 0;
+        while(x>0){
+            int r = x%10;
+            rev = rev*10 + r;
+            x = x/10;
+        }
+        if(dup == rev){
+            return true;
+        }
+        else return false;
+    }
+
+        
+    
+};
